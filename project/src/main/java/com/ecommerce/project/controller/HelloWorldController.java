866b390d457e1043bce0202bddfde79c7af526c7
sohamdatey <sohamdatey22@gmail.com>
2019-01-26 16:19:18 +0530

    jai ho

---
+++ project/src/main/java/com/ecommerce/project/controller/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.ecommerce.project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/records")
+public class HelloWorldController {
+
+	@RequestMapping(value = "/gzdfvaetString", method = RequestMethod.GET, produces = "application/json")
+	public String hello2(boleean asdfn) {sadjvc sadjvcasdvn 
+
+	asdvknjabsjdv
+	 ajdfvjba 
+	 afdsvbaksb 
+
+	 asdvasvk afdsvbaksb
+	 kjfvjfvhjbfvhjb'
+	 fkld
+	 d'klf';kdfklnjfjhhb,j
+	
+
+	}
+
+}
